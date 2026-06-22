fe4a8951fe6f7c8c09f98dc69c05dcf28e01742d
YCBEN <yacinebendou@gmail.com>
2026-06-22 22:29:06 +0100

    the safearea also can be used to wrap thhe scaffold widget and save the top and bottom sides

--- Leaning/flutter_app/lib/main.dart
+++ Leaning/flutter_app/lib/main.dart
@@ -19,41 +19,41 @@ class MyApp extends StatelessWidget {
         ),
       ),
 
-      home: Scaffold(
-        appBar: AppBar(title: Text("Flutter APP"), centerTitle: true),
-        drawer: SafeArea(
-          child: Drawer(
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(title: Text("Flutter APP"), centerTitle: true),
+          drawer: Drawer(
             child: Column(
               children: [
                 // DrawerHeader(
                 //   child: Text ('Drawer'), 
                 // ),  
                 
-          
+        
                 ListTile(
                   title: Text('Logout')
                 )
               ]
             )
           ),
-        ),
-        floatingActionButton: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)),
-            SizedBox(height: 10.0),
-            FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)),
-          ],
-        ),
-        bottomNavigationBar: NavigationBar(
-          destinations: [
-            NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
-            NavigationDestination(icon: Icon(Icons.person), label: 'Profile'),
-          ],
-          onDestinationSelected: (int value) {
-            print(value);
-          },
-          selectedIndex: 0,
+          floatingActionButton: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)),
+              SizedBox(height: 10.0),
+              FloatingActionButton(onPressed: () {}, child: Icon(Icons.add)),
+            ],
+          ),
+          bottomNavigationBar: NavigationBar(
+            destinations: [
+              NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+              NavigationDestination(icon: Icon(Icons.person), label: 'Profile'),
+            ],
+            onDestinationSelected: (int value) {
+              print(value);
+            },
+            selectedIndex: 0,
+          ),
         ),
       ),
     );
