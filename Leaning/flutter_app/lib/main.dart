5aa8437032a7c75388af33c312b18a86644a1b74
YCBEN <yacinebendou@gmail.com>
2026-06-26 00:59:05 +0100

    small challange making the light and dark modes logic with valueNotifier and valueListrnableBuilder

--- Leaning/flutter_app/lib/main.dart
+++ Leaning/flutter_app/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/data/notifiers.dart';
 import 'package:flutter_app/views/pages/widget_tree.dart';
-
 
 void main() {
   runApp(const MyApp());
@@ -14,17 +14,22 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal,
-             brightness: Brightness.dark),
-   
-      ),
-      home: WidgetTree()
+    return ValueListenableBuilder(
+      valueListenable: isDarkModeNotifier,
+      builder: (context, isDarkMode, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(
+              seedColor: Colors.teal,
+              brightness: isDarkMode ? Brightness.dark : Brightness.light,
+            ),
+          ),
+          home: WidgetTree(),
+        );
+      },
     );
   }
 }
--- Leaning/flutter_app/lib/views/pages/widget_tree.dart
+++ Leaning/flutter_app/lib/views/pages/widget_tree.dart
@@ -13,7 +13,22 @@ class WidgetTree extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Flutter APP')),
+      appBar: AppBar(
+        title: Text('Flutter APP'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              isDarkModeNotifier.value = !isDarkModeNotifier.value;
+            },
+            icon: ValueListenableBuilder(
+              valueListenable: isDarkModeNotifier,
+              builder: (context, isDarkMode, child) {
+                return Icon(isDarkMode ? Icons.light_mode : Icons.dark_mode);
+              },
+            ),
+          ),
+        ],
+      ),
 
       body: ValueListenableBuilder(
         valueListenable: selectedPageNotifier,
