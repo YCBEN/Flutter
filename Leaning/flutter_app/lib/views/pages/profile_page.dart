27aebec739b57a17cba01f10ff8a95219f0d873f
YCBEN <yacinebendou@gmail.com>
2026-06-26 18:21:55 +0100

    used the TextField

--- Leaning/flutter_app/lib/views/pages/profile_page.dart
+++ Leaning/flutter_app/lib/views/pages/profile_page.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
   @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  String name = "";
+  @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Profile Page'),);
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Center(child: Text("Hello !")),
+
+          TextField(
+            decoration: InputDecoration(
+              border: UnderlineInputBorder(),
+              hintText: "Name",
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
