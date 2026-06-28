3830db6d72933de2efab7489bf6810c5ad87b830
YCBEN <yacinebendou@gmail.com>
2026-06-28 20:40:54 +0100

    used the animations with lotties pacjage and used the fitedbox

---
+++ Leaning/flutter_app/lib/data/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class KTextStyle {
+  static const TextStyle titleTextStyle = TextStyle(
+    color: Colors.teal,
+    fontSize: 18.0,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle descriptionTextStyle = TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.w100,
+  );
+}
